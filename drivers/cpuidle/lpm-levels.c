247f96c677c85b39c3da2eef3b1c9cfb87531b5d
Maulik Shah <mkshah@codeaurora.org>
2015-07-21 10:33:34 +0530

    cpuidle: lpm-levels: Rename cpumask variable name
    Current name is detected as misspelled name in the automation.
    Rename cpumask variable name to num_children_in_sync.
    
    Change-Id: I104b65ae5a25b793a0e1df2154d73d254fcec6a3
    Signed-off-by: Maulik Shah <mkshah@codeaurora.org>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -389,7 +389,7 @@ static uint64_t get_cluster_sleep_time(struct lpm_cluster *cluster,
 	}
 
 	cpumask_and(&online_cpus_in_cluster,
-			&cluster->num_childs_in_sync, cpu_online_mask);
+			&cluster->num_children_in_sync, cpu_online_mask);
 
 	for_each_cpu(cpu, &online_cpus_in_cluster) {
 		td = &per_cpu(tick_cpu_device, cpu);
