624beebdbc41cf9c68162fa326988c9964f5a467
yarpiin <yarpiin@gmail.com>
2016-07-18 23:24:10 +0100

    cpufreq: ad Electroactive CPU gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -462,6 +462,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTROACTIVE)
+extern struct cpufreq_governor cpufreq_gov_electroactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electroactive)
 #endif
 
 /*********************************************************************
