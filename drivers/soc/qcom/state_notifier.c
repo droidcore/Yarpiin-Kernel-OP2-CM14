ea6ee2f3436614d4a1404f2563eec01ea31cdf45
yarpiin <yarpiin@gmail.com>
2016-08-08 21:43:33 +0100

    Revert "drivers: state_notifier: use more relaxed workqueu but still high priority"
    This reverts commit 61e3ebafabd2ab9eb222b10bf865dfa866791474.

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -113,17 +113,7 @@ void state_resume(void)
 
 static int __init state_notifier_init(void)
 {
-	int ret;
-
-	notif.notifier_call = fb_notifier_callback;
-	ret = fb_register_client(&notif);
-	if (ret)
-		pr_err("Failed to register FB notifier callback for state notifier.\n");
-
-	susp_wq =
-		alloc_workqueue("state_susp_wq", WQ_HIGHPRI | WQ_UNBOUND |
-					WQ_FREEZABLE | WQ_MEM_RECLAIM, 0);
-
+	susp_wq = create_singlethread_workqueue("state_susp_wq");
 	if (!susp_wq)
 		pr_err("State Notifier failed to allocate suspend workqueue\n");
 
